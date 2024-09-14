3330f45d50edbc36fc399601c6f9b267cd816906
lalawu9453 <lalawu499@gmail.com>
2024-09-14 10:03:50 +0800

    Update FGR.js

--- FGR.js
+++ FGR.js
@@ -1,6 +1,6 @@
 
 var body = $response.body;
-
+/*
 // We have not performed request link comparison.
 var obj = JSON.parse(body);
 // rewrite
@@ -8,7 +8,7 @@ obj = JSON.parse(body);
 obj = JSON.parse($response.body);
 obj.data.ITEM_ID = "yubi10006";
 body = JSON.stringify(obj);
-
+*/
 $done({body});
 
 
