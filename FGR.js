f960ec7a982a3cd08083e024d1f728f1bb28195d
lalawu9453 <lalawu499@gmail.com>
2024-09-14 09:39:19 +0800

    Update FGR.js
    re new

--- FGR.js
+++ FGR.js
@@ -1,18 +1,13 @@
 var body = $response.body;
-//我们就没有进行请求链接匹配了
+// We have not performed request link comparison.
 var obj = JSON.parse(body);
-//修改
- obj = JSON.parse(body);
- obj = JSON.parse($response.body);
+// rewrite
+// obj = JSON.parse(body);
+// obj = JSON.parse($response.body);
 obj.data.ITEM_ID = "yubi10006";
 body = JSON.stringify(obj);
 $done({body});
-/*
-参数说明
-title: 主标题
-subtitle: 副标题
-body: 消息正文
-*/
 
-$notification.post("Storm Sniffer","Network Debug Tool","最后的荣光充值修改成功!!!")
+
+$notification.post("Storm Sniffer","Network Debug Tool","FGR Get it!!!")
 
